9fe3c4376fa42736e59009bac97d252ddf90fc2d
Max Kassander <maximilian.kassander@gmail.com>
2021-05-25 00:40:03 +0200

    Setup?

---
+++ nim.cpp
@@ -0,0 +1,19 @@
+using namespace marczaku-gp20-2021-0524-nim-game-MKassander
+
+int main() {
+	int number = 24;
+	
+	cout << "Number of matches" << number << endl;
+
+
+}
+
+/*# Nim Game
+
+- 2 Players
+- 24 matches on the table
+- players take turns to draw matches
+- may draw 1, 2 or 3 matches(not more, not less)
+- player who has to take last match loses(alternatively: wins)
+- make an ASCII - Art Display of the remaining matches `||||||||||||||||`
+- implement AI - Player :)*/
