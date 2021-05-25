1ccc7b617761dc0eecca2d6bb756c1b31d87862c
Max Kassander <maximilian.kassander@gmail.com>
2021-05-25 19:27:00 +0200

    Prints a field and a symbol can be entered into it

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,20 +1,48 @@
-// TicTacToe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string player1Symbol = "X";
+    string player2Symbol = "O";
+
+    string squares[9] = {"?", "?", "?", "?", "?", "?", "?", "?", "?"};
+
+    std::cout << "Welcome to TicTacToe!\n";
+
+    
+
+    while (true)
+    {
+        cout << squares[0] << " " << squares[1] << " " << squares[2] << endl;
+        cout << squares[3] << " " << squares[4] << " " << squares[5] << endl;
+        cout << squares[6] << " " << squares[7] << " " << squares[8] << endl;
+
+        cout << "Enter a numer (0-8) to place your pawn" << endl;
+        int response;
+        cin >> response;
+        if (squares[response] == "?") {
+            squares[response] = player1Symbol;
+        }
+        else cout << "Occupied space, try again" << endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*
+2 Players
+Player 1: X
+Player 2: O
+3x3 Grid - we have not learned about Arrays, yet, but for 3x3, it can be done otherwise, too :)
+players take turns choosing an empty grid cell and putting their symbol into it
+player that has three of his symbols either
+horizontally
+vertically
+diagonally wins and the game ends instantly.
+make an ASCII-Art Display of the grid
+ | |X
+-----
+O|X|
+-----
+O| |
+implement AI-Player :)
+*/
