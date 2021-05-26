f9bc7aae14cb76b40f6355a2f480dc5c27d9045d
Max Kassander <maximilian.kassander@gmail.com>
2021-05-26 18:27:02 +0200

    Binary setup

---
+++ BinaryConverter/BinaryConverter/BinaryConverter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+/*
+Write a Program that reads (cin) decimal Integer numbers (positive only) And Oututs the Binary representation of that number: 0b110
+
+Firstly, implement the function the way we know, with division and modulo.
+Secondly, implement the function using binary operations.
+Tip: Both exercises will be a lot easier, if you output one character at a time!
+*/
