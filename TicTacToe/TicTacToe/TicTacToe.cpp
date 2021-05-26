82b0f5644a92d81be30c4a2c7c59e4eefbe95579
Max Kassander <maximilian.kassander@gmail.com>
2021-05-26 15:58:14 +0200

    basic 2 player working

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 using namespace std;
 
+void takeTurn(string player, string squares[9]) {
+    cout << player << "'s turn" << endl;
+    cout << "Enter a numer (0-8) to place your pawn" << endl;
+    int response;
+    cin >> response;
+    if (squares[response] == "?") {
+        squares[response] = player;
+
+        if (squares[0] == player && squares[1] == player && squares[2] == player ||
+            squares[3] == player && squares[4] == player && squares[5] == player ||
+            squares[6] == player && squares[7] == player && squares[8] == player ||
+
+            squares[0] == player && squares[3] == player && squares[6] == player ||
+            squares[1] == player && squares[4] == player && squares[7] == player ||
+            squares[2] == player && squares[5] == player && squares[8] == player ||
+
+            squares[0] == player && squares[4] == player && squares[8] == player ||
+            squares[2] == player && squares[4] == player && squares[6] == player)
+        {
+            cout << "You win!" << endl;
+        }
+    }
+    else cout << "Occupied space, try again" << endl;
+}
+
 int main()
 {
     string player1 = "X";
     string player2 = "O";
+    bool player1Turn = true;
 
     string squares[9] = {"?", "?", "?", "?", "?", "?", "?", "?", "?"};
 
@@ -16,33 +42,20 @@ int main()
         cout << squares[3] << " " << squares[4] << " " << squares[5] << endl;
         cout << squares[6] << " " << squares[7] << " " << squares[8] << endl;
 
-        cout << "Enter a numer (0-8) to place your pawn" << endl;
-        int response;
-        cin >> response;
-        if (squares[response] == "?") {
-            squares[response] = player1;
-
-            if (squares[0] == player1 && squares[1] == player1 && squares[2] == player1 || 
-                squares[3] == player1 && squares[4] == player1 && squares[5] == player1 ||
-                squares[6] == player1 && squares[7] == player1 && squares[8] == player1 ||
-
-                squares[0] == player1 && squares[3] == player1 && squares[6] == player1 ||
-                squares[1] == player1 && squares[4] == player1 && squares[7] == player1 ||
-                squares[2] == player1 && squares[5] == player1 && squares[8] == player1 ||
-
-                squares[0] == player1 && squares[4] == player1 && squares[8] == player1 ||
-                squares[2] == player1 && squares[4] == player1 && squares[6] == player1)
-            {
-                cout << "You win!" << endl;
-            }
+        switch (player1Turn)
+        {
+        case true:
+            takeTurn(player1, squares);
+            break;
+        case false:
+            takeTurn(player2, squares);
+            break;
         }
-        else cout << "Occupied space, try again" << endl;
+        player1Turn = !player1Turn;
     }
 }
 
-void takeTurn(string player) {
 
-}
 
 /*
 2 Players
